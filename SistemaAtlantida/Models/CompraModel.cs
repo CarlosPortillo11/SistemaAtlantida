4d0180ca623bf99c121a89531f935490e628db19
Carlos Portillo <carloskportillo@gmail.com>
2023-11-09 14:24:35 -0600

    Inicio del desarrollo del formulario de compras

---
+++ SistemaAtlantida/Models/CompraModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaAtlantida.Models
+{
+    public class CompraModel
+    {
+        [Key]
+        public string? NumeroTarjeta { get; set; }
+
+        public DateTime Fecha { get; set; }
+        public string? Descripcion { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
