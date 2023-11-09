f545923349178a785cdf87f48d9f328428d147fd
Carlos Portillo <carloskportillo@gmail.com>
2023-11-09 10:02:29 -0600

    Agregar archivos de proyecto.

---
+++ SistemaAtlantida/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaAtlantida.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
